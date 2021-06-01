2fdaeb250f08954819e5d600842026e56b36b079
AnggaWikaNugraha <anggawika18@gmail.com>
2021-06-01 13:56:06 +0700

    edit books

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -50,7 +50,7 @@
                             <td>{{ $book->stock }}</td>
                             <td>{{ $book->price }}</td>
                             <td>
-                                [TODO: actions]
+                                <a href="{{ route('books.edit', [$book->id]) }}" class="btn btn-info btn-sm"> Edit </a>
                             </td>
                         </tr>
                     @endforeach
