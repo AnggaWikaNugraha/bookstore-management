8c88bec115e4030283365b88297590d6f77ab0c0
AnggaWikaNugraha <anggawika18@gmail.com>
2021-06-01 12:29:49 +0700

    migrate book_category

--- database/migrations/2021_06_01_051120_create_books_table.php
+++ database/migrations/2021_06_01_051120_create_books_table.php
@@ -14,6 +14,7 @@ class CreateBooksTable extends Migration
     public function up()
     {
         Schema::create('books', function (Blueprint $table) {
+            $table->id();
             $table->string('title');
             $table->string('slug');
             $table->text('description');
